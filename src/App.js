054135c3b0d4f0c185d564477f0a3f9c10fca10a
sonaliverma82276 <60004256+sonaliverma82276@users.noreply.github.com>
2021-07-16 10:33:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
+import Home from './components/Home/home';
+import Track from './components/Track/Track';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import trace from './components/Track/trace';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/track" exact component={Track} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/signup" exact component={Signup} />
+      <Route path="/join" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+      <Route path="/trace" component={trace} />
+    </Router>
+  );
+}
+
+export default App;
